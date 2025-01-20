b39aab8de1e68413f13b4dba7be43151220d042f
Fkirol <lukushikirol@gmail.com>
2025-01-19 21:11:38 -0500

    juan

--- Usuario/models.py
+++ Usuario/models.py
@@ -41,7 +41,7 @@ class Post(models.Model):
     suggests = models.ManyToManyField('self', symmetrical=False, blank=True)
     status = models.IntegerField(choices=Status.choices, default=Status.UNPUBLISHED)
     date_time = models.DateTimeField(auto_now_add=True)
-    featured_image = models.CharField(max_length=200,default="http://media.cubadebate.cu/wp-content/gallery/autores/oscar-figueredo-reinaldo.jpg",null=True)
+    featured_image = models.CharField(max_length=200,default="http://media.cubadebate.cu/wp-content/gallery/autores/oscar-figueredo-reinaldo.jpg")
         
     def __str__(self):
         return self.title
